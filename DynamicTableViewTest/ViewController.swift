31b15d8a916704df4fefe13b94085e2f4d2b97dd
Maximilian Berndt <maximilian.berndt@foodora.com>
2017-12-04 15:36:13 +0100

    Initial Commit

---
+++ DynamicTableViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicTableViewTest
+//
+//  Created by Maximilian Berndt on 04.12.17.
+//  Copyright © 2017 mb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
